62b730ee73745ad2e1102d6463bb903c29c75747
Dhineshkumar <dhinesh886@gmail.com>
2024-06-06 10:50:57 +0530

    after testing sir

--- lib/honor_slip_history.dart
+++ lib/honor_slip_history.dart
@@ -146,10 +146,9 @@ class _HonorHistoryState extends State<HonorHistory> {
                                   title : data[i]["Tomobile"] != fetchMobile ? Text(
                                        "${data[i]["Toname"]}") :
                                   Text(" ${data[i]["name"]}"),
-
-
-
-
+                                trailing: data[i]["Tomobile"] != fetchMobile
+                                    ? Icon(Icons.call_received, color: Colors.green[800])
+                                    : Icon(Icons.call_made, color: Colors.red),
                                   children:[ Column(
                                     // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                     children:  [
@@ -164,17 +163,7 @@ class _HonorHistoryState extends State<HonorHistory> {
                                       const SizedBox(height: 10,),
                                       data[i]['businessName'].isNotEmpty
                                           ? ListTile(
-                                        title: Text("   Referee Name : ${data[i]["businessName"]}",style: TextStyle(fontWeight: FontWeight.bold),),
-                                        trailing: Row(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: [
-                                            Card(
-                                              child: data[i]["Tomobile"] == fetchMobile
-                                                  ? Icon(Icons.call_received, color: Colors.green[800])
-                                                  : Icon(Icons.call_made, color: Colors.red),
-                                            ),
-                                          ],
-                                        )
+                                        title: Text("   Customer Name : ${data[i]["businessName"]}",style: TextStyle(fontWeight: FontWeight.bold),),
                                       ) : Container(),
                                       const SizedBox(height: 5,),
                                       Row(
@@ -217,7 +206,7 @@ class _HonorHistoryState extends State<HonorHistory> {
                                           ]
                                       ),
                                     ],
-                                  ),]
+                                  ),],
                               ),
                             ),
                           ],
