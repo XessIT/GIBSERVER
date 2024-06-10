f3bbeb7af5aedcdf7333f6424bff92e732386172
ELANGOMANIS <elangokaninfotech@gmail.com>
2024-06-10 14:56:34 +0530

    git elango

--- lib/home.dart
+++ lib/home.dart
@@ -19,7 +19,6 @@ import 'meeting.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 
 class Homepage extends StatefulWidget {
@@ -562,19 +561,6 @@ class _HomepageState extends State<Homepage> {
   }
 
   //download image
-  Future<void> _downloadImage(String url) async {
-    var status = await Permission.storage.request();
-    if (status.isGranted) {
-      final directory = await getExternalStorageDirectory();
-      final filePath = '${directory?.path}/${url.split('/').last}';
-      final response = await http.get(Uri.parse(url));
-      final file = File(filePath);
-      await file.writeAsBytes(response.bodyBytes);
-      print('Image downloaded to $filePath');
-    } else {
-      print('Permission denied');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
